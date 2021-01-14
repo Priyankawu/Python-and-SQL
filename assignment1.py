9e7437077a76925c0e2d95612a692edfaee59997
Priyanka Thakan <priyankawu@yahoo.com>
2021-01-14 14:17:31 -0800

    Update one tuple with trailing comma

--- assignment1.py
+++ assignment1.py
@@ -3,6 +3,7 @@ import sqlite3
 
 #create a database
 conn = sqlite3.connect('test.db')
+#This is a tuple. To add only one item, it must have a , at the end
 fileList = ('information.docx','Hello.txt','myImage.png', \
             'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg')
 
@@ -19,7 +20,8 @@ with conn:
     for i in fileList:
         if i.endswith('txt'):
             print(i)
-            cur.execute("INSERT INTO tbl(col_fname) VALUES(?)", (i))
+            # note the comma after , : (i,) for one item in tuple
+            cur.execute("INSERT INTO tbl(col_fname) VALUES(?)", (i,))
     conn.commit()
     
 conn.close()
